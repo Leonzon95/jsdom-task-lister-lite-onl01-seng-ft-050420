8ae7a230d737b067208761762762e77578f50d33
leobr <leobrionesherrera@gmail.com>
2020-08-07 16:51:39 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    let input = document.getElementById('new-task-description').value;
+    const li = document.createElement('li');
+    const tasks = document.querySelector('#list');
+    li.innerHTML = input; 
+    tasks.appendChild(li);
+    input = "";
+  });
+
+
 });
